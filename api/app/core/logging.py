b7f402c604af35c2e0e47c002db2e130d3854688
gabrielclcardoso <gabrielclcardoso@gmail.com>
2026-04-20 18:55:27 -0300

    feat: implement base template for user action logging

--- api/app/core/logging.py
+++ api/app/core/logging.py
@@ -4,5 +4,5 @@ import logging
 def setup_logging() -> None:
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+        format="%(asctime)s %(levelname)s [%(name)s] %(message)s",
     )
