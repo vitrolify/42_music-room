9860043cb6ead3892b5c285ee6e89c116ceca4df
gabrielclcardoso <gabrielclcardoso@gmail.com>
2026-04-23 17:32:09 -0300

    feat: implement grafana dashboard
    Enable visualization of both logs and metrics with grafana prometheus
    loki and promtail

--- api/app/observability/metrics.py
+++ api/app/observability/metrics.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from prometheus_client import Info
 from prometheus_fastapi_instrumentator import Instrumentator
 
 
-# Can be used to configure custom metrics later in development
 def setup_metrics(app: FastAPI) -> None:
-    Instrumentator().instrument(app).expose(app)
+    i = Info("app_info", "FastAPI application information", namespace="fastapi")
+    i.info({"app_name": "vitrolify-api"})
+
+    instrumentator = Instrumentator(
+        should_group_status_codes=False,
+        should_ignore_untemplated=True,
+        should_respect_env_var=True,
+        should_instrument_requests_inprogress=True,
+        excluded_handlers=["/metrics"],
+        inprogress_name="fastapi_requests_in_progress",
+        inprogress_labels=True,
+    )
+
+    _ = instrumentator.instrument(app).expose(app)
