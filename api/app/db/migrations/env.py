0c1de93cb9d8907c4407bed6bb1ebd85d2b99f00
Pedro Botelho <pedrobotelhodev@gmail.com>
2026-05-03 16:50:44 -0300

    fix: ignore intentional models import in alembic env

--- api/app/db/migrations/env.py
+++ api/app/db/migrations/env.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from app.core.config import settings
 from app.db.base import Base
-from app import models
+from app import models # noqa: F401
 
 
 config = context.config
