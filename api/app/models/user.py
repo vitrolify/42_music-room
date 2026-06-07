8a9a9aef4f77dd052e6cfe28d4d192fe96309994
gabrielclcardoso <gabrielclcardoso@gmail.com>
2026-06-07 14:10:21 -0300

    feat: create users table through alembic

--- api/app/models/user.py
+++ api/app/models/user.py
@@ -9,13 +9,11 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(
         primary_key=True,
-        index=True,
     )
 
     email: Mapped[str] = mapped_column(
         String(255),
         unique=True,
-        index=True,
         nullable=False,
     )
 
